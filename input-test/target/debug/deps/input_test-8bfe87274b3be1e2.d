94af582a9f30b4a0219fc473ed2dc07cec502acc
toguri <oguri_toru@cyberagent.co.jp>
2025-04-18 23:58:38 +0900

    Update .gitignore

---
+++ input-test/target/debug/deps/input_test-8bfe87274b3be1e2.d
@@ -0,0 +1,5 @@
+/Users/a13694/usr/src/rust-hello/input-test/target/debug/deps/input_test-8bfe87274b3be1e2.rmeta: src/main.rs
+
+/Users/a13694/usr/src/rust-hello/input-test/target/debug/deps/input_test-8bfe87274b3be1e2.d: src/main.rs
+
+src/main.rs:
---
+++ input-test/target/debug/deps/lazy_static-08782d17319ee4ed.d
@@ -0,0 +1,6 @@
+/Users/a13694/usr/src/rust-hello/input-test/target/debug/deps/lazy_static-08782d17319ee4ed.rmeta: /Users/a13694/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /Users/a13694/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/a13694/usr/src/rust-hello/input-test/target/debug/deps/lazy_static-08782d17319ee4ed.d: /Users/a13694/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /Users/a13694/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
+
+/Users/a13694/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs:
+/Users/a13694/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs:
